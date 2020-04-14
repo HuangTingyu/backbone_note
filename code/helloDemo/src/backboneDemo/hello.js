d80847a5b2795de5094aff868e6265c9fc374d4a
sakurahuang <100728545@qq.com>
2020-04-14 17:54:11 +0800

    hello

---
+++ code/helloDemo/src/backboneDemo/hello.js
@@ -0,0 +1,31 @@
+var $ = require('jquery')
+var _ = require('underscore')
+var Backbone = require('backbone')
+var hello = $(function(){
+    // 定义模型类
+    window.Test = Backbone.Model.extend({
+        defaults:{
+            content:''
+        }
+    })
+    // 创建集合模式类
+    window.TestList = Backbone.Collection.extend({
+        model:Test
+    })
+    // 向模型添加数据
+    var data = new TestList({
+        content:'hello,backbone'
+    })
+    // 创建View对象
+    window.TestView = Backbone.View.extend({
+        el:$("body"),
+        initialize:function(){
+            $('#root').html(data.models[0].get("content"))
+        }
+    })
+    // 实例化View对象
+    window.App = new TestView
+
+})
+
+module.exports = hello;
